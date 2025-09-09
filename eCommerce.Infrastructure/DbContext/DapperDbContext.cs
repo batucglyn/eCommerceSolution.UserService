c78249c412a3bc8a71ef81bfd132afb26a9ea3d8
batucglynn <batuhancglyn@gmail.com>
2025-09-10 02:45:10 +0300

    docker configure

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -17,7 +17,10 @@ public class DapperDbContext
     public DapperDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
-        string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+        string connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+        string connectionString = connectionStringTemplate
+          .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+          .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
 
         //Create a new NpgsqlConnection with the retrieved connection string
         _connection = new NpgsqlConnection(connectionString);
